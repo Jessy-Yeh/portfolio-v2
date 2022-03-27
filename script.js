a8e3060d4fcee0268486b08c66022c9ec33042b6
Jessy Yeh <jiejun.yeh@gmail.com>
2022-03-27 15:16:41 +0100

    Highlight nav when scrolling to different sections

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menuIcon = document.querySelector(".menu");
 const menuContent = document.querySelector(".menu-content-container");
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .menu-content-container ul li");
 
 menuIcon.addEventListener("click", () => {
   menuContent.classList.toggle("display");
@@ -7,4 +9,22 @@ menuIcon.addEventListener("click", () => {
 
 menuContent.addEventListener("click", () => {
   menuContent.classList.toggle("display");
+});
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("current");
+    if (li.classList.contains(current)) {
+      li.classList.add("current");
+    }
+  });
 });
